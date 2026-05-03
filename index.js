6d1665b604dd1e36e9b724e03b9637cc40ef9d4f
ghislaintankat-cyber <ghislaintankat@gmail.com>
2026-05-04 00:16:12 +0100

    Delete index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-exports.newJobNotification = functions.database.ref('/jobs/{jobId}')
-.onCreate((snapshot, context) => {
-   // Notify nearby users
-});
-
-exports.cleanExpiredJobs = functions.pubsub.schedule('every 24 hours')
-.onRun(() => {
-   // Remove expired jobs
-});
